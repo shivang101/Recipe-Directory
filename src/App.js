cdb878343a5af9ce31c790ecd4b92ab567abfde3
shivang <shivangmathur101@gmail.com>
2022-04-10 22:29:11 +0530

    inital files

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+//page components
+import Home from "./Pages/home/Home";
+import Create from "./Pages/create/Create";
+import Search from "./Pages/search/Search";
+import Recipe from "./Pages/recipe/Recipe";
+
+//styles
+import "./App.css";
+
+function App() {
+  return <div className="App"></div>;
+}
+
+export default App;
