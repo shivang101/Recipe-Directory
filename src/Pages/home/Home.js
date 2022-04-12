80988f0af7bd0d0041ae43230fbe94040a466082
Shivang <shivangmathur101@gmail.com>
2022-04-13 00:52:27 +0530

    Refactored HOME Page

--- src/Pages/home/Home.js
+++ src/Pages/home/Home.js
@@ -1,7 +1,10 @@
+//styling
 import "./Home.css";
 
-import React from "react";
+//hooks
 import { useFetch } from "../../hooks/useFetch";
+//components
+import RecipeList from "../../components/RecipeList";
 
 export default function Home() {
   const { data, isPending, error } = useFetch("http://localhost:3000/recipes");
@@ -11,7 +14,7 @@ export default function Home() {
       {error && <p className="error">{error}</p>}
       {isPending && <p className="loading">Loading...</p>}
 
-      {data && data.map((el) => <h2 key={el.id}>{el.title}</h2>)}
+      {data && <RecipeList recipes={data} />}
     </div>
   );
 }
