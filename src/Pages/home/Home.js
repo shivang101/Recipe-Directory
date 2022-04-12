e12b46d6d387499d2b61e7fa19d861a30b793f84
Shivang <shivangmathur101@gmail.com>
2022-04-13 00:20:41 +0530

    added fetch hook and fetched data for home page

--- src/Pages/home/Home.js
+++ src/Pages/home/Home.js
@@ -1,7 +1,17 @@
 import "./Home.css";
 
 import React from "react";
+import { useFetch } from "../../hooks/useFetch";
 
 export default function Home() {
-  return <div>Home</div>;
+  const { data, isPending, error } = useFetch("http://localhost:3000/recipes");
+
+  return (
+    <div className="home">
+      {error && <p className="error">{error}</p>}
+      {isPending && <p className="loading">Loading...</p>}
+
+      {data && data.map((el) => <h2 key={el.id}>{el.title}</h2>)}
+    </div>
+  );
 }
