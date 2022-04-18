e31b88a7312f3f34b70267106208b302c0d1e503
Shivang <shivangmathur101@gmail.com>
2022-04-19 00:11:05 +0530

    added form of new recipe

--- src/Pages/create/Create.js
+++ src/Pages/create/Create.js
@@ -1,7 +1,53 @@
+import { useState } from "react";
+
+//styles
 import "./Create.css";
 
-import React from "react";
-
 export default function Create() {
-  return <div>Create</div>;
+  const [title, setTtile] = useState("");
+  const [method, setMethod] = useState("");
+  const [cookingTime, setCookingTime] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(title, method, cookingTime);
+  };
+
+  return (
+    <div className="create">
+      <h2 className="page-title">Add a new Recipe</h2>
+      <form className="create-form" onSubmit={handleSubmit}>
+        <label>
+          <span>Recipe Title:</span>
+          <input
+            type="text"
+            onChange={(e) => setTtile(e.target.value)}
+            value={title}
+            required
+          />
+        </label>
+        <label>
+          <span>
+            Recipe Method:
+            <textarea
+              onChange={(e) => setMethod(e.target.value)}
+              value={method}
+              required
+            />
+          </span>
+        </label>
+
+        <label>
+          <span>Cooking Time:</span>
+          <input
+            type="number"
+            onChange={(e) => setCookingTime(e.target.value)}
+            required
+            value={cookingTime}
+          />
+        </label>
+        <button className="btn">Submit</button>
+      </form>
+    </div>
+  );
 }
--- src/Pages/recipe/Recipe.js
+++ src/Pages/recipe/Recipe.js
@@ -13,8 +13,19 @@ export default function Recipe() {
     <div className="recipe">
       {error && <p className="error">{error}</p>}
       {isPending && <p className="loading">Loading...</p>}
+      {recipe && (
+        <>
+          <h2 className="page-title">{recipe.title}</h2>
+          <p>Takes {recipe.cookingTime} to cook.</p>
 
-      {recipe && <h1>{recipe.title}</h1>}
+          <ul>
+            {recipe.ingredients.map((ingredient) => (
+              <li key={ingredient}>{ingredient}</li>
+            ))}
+          </ul>
+          <p className="method">{recipe.method}</p>
+        </>
+      )}
     </div>
   );
 }
