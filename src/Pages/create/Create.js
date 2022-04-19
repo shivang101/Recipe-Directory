8c3e2924908a9a064721aecd93ffbeb9cce7e8ba
Shivang <shivangmathur101@gmail.com>
2022-04-19 20:14:49 +0530

    implemented write operation in json server

--- src/Pages/create/Create.js
+++ src/Pages/create/Create.js
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
+import { useFetch } from "../../hooks/useFetch";
 
 //styles
 import "./Create.css";
@@ -7,10 +8,37 @@ export default function Create() {
   const [title, setTtile] = useState("");
   const [method, setMethod] = useState("");
   const [cookingTime, setCookingTime] = useState("");
+  const [newIngredient, setNewIngredient] = useState("");
+  const [ingredients, setIngredients] = useState([]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(title, method, cookingTime);
+    console.log(title, method, cookingTime, ingredients);
+    newRecipeData({
+      title,
+      ingredients,
+      method,
+      cookintTime: `${cookingTime} minutes`,
+    });
+  };
+
+  const {
+    postData: newRecipeData,
+    data,
+    error,
+  } = useFetch("http://localhost:3000/recipes", "POST");
+
+  const ingInput = useRef(null);
+
+  const handleAdd = (e) => {
+    e.preventDefault();
+    const ing = newIngredient.trim();
+
+    if (ing && !ingredients.includes(ing)) {
+      setIngredients((prevIngredients) => [...prevIngredients, ing]);
+    }
+    setNewIngredient("");
+    ingInput.current.focus();
   };
 
   return (
@@ -24,8 +52,32 @@ export default function Create() {
             onChange={(e) => setTtile(e.target.value)}
             value={title}
             required
+            placeholder="Enter a Title"
           />
         </label>
+
+        <label>
+          <span>Recipe Ingredients:</span>
+          <div className="ingredients">
+            <input
+              type="text"
+              onChange={(e) => setNewIngredient(e.target.value)}
+              value={newIngredient}
+              ref={ingInput}
+            />
+            <button onClick={handleAdd} className="btn">
+              {" "}
+              Add
+            </button>
+          </div>
+        </label>
+        <p>
+          current ingredients:{" "}
+          {ingredients.map((el) => (
+            <em key={el}>{el}, </em>
+          ))}
+        </p>
+
         <label>
           <span>
             Recipe Method:
